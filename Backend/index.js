59a1889cb12b44236889abcabdb8749b8337e753
sanchita-sm <sanchita@ku.th>
2026-01-30 11:59:24 +0700

    add Frontend & Backend

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+//ทำการ import โมดูล http
+const http = require('http');
+const host = 'localhost';
+const port = 8000;
+
+//กำหนดค่า server
+const requestListener = function (req,res) {
+    res.writeHead(200);
+    res.end('Hello, World! This is my server.');
+}
+//run server
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+})
