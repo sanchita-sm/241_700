c3a33666f097c3f1c275a7ac3f87615a8d19f937
sanchita-sm <sanchita@ku.th>
2026-02-27 13:47:30 +0700

    add api get post put delete

--- Backend/index.js
+++ Backend/index.js
@@ -16,22 +16,130 @@ server.listen(port, host, () => {
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise')
 const app = express();
 
 app.use(bodyParser.json());
 
 const port = 8000;
 
-let users = [];
+/**
+//การเขียนแบบที่ 1
+app.get('/testdb', (req,res) => {
+    mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8700
+    }).then((conn) => {
+        conn
+        .query('SELECT * FROM users')
+        .then((results) => {
+            res.json(results[0]);
+        }).catch((err) => {
+            res.json({ error: err.message});
+        });
+    })
+})
+*/
+let conn = null;
+const initMySQL = async () => {
+    conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8700
+    });
+    console.log('Connected to MySQL database');
+}
+
+//path: = GET /users/:id สำหรับดึงข้อมูล users ทั้งหมด
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
+})
+
+//path: = POST /users/:id สำหรับเพิ่ม user ใหม่
+app.post('/users', async (req, res) => {
+    try {
+        let user = req.body;
+        const results = await conn.query('INSERT INTO users SET ?', user);
+        res.json({
+            message: 'User added successfully',
+            data: results[0]
+        });
+    } catch (error) {
+        console.error('Error inserting user:' ,error);
+        res.status(500).json({ message: 'Error adding user'});
+    }
+})
+
+//path: = GET /users/:id สำหรับดึงข้อมูล user ตาม id
+app.get('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        const results = await conn.query('SELECT * FROM users WHERE id = ?', id);
+        if (results[0].length === 0) {
+            throw { statusCode : 404, message: 'User not found'};
+        }
+        res.json(results[0][0]);
+    } catch (error) {
+        console.error('Error fetching user:' ,error);
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message: error.message || 'Error fetching user'});
+    }
+})
+
+//path: = PUT /users/:id สำหรับอัพเดทข้อมูล user ตาม id
+app.put('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        let updateUser = req.body;
+        const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updateUser,id]);
+        res.json({
+            message: 'User updated successfully',
+            data: results[0]
+        })
+    } catch (error) {
+        console.error('Error updating user:' ,error);
+        res.status(500).json({ message: 'Error updating user'});
+    }
+})
+
+//path: = DELETE /users/:id สำหรับลบข้อมูล user ตาม id
+app.delete('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        const results = await conn.query('DELETE FROM users WHERE id = ?', id);
+        res.json({
+            message: 'User deleted successfully',
+            data: results[0]
+        })
+    } catch (error) {
+        console.error('Error deleting user:' ,error);
+        res.status(500).json({ message: 'Error deleting user'});
+    }
+})
+
+app.listen(port, async () => {
+    await initMySQL();
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+/**let users = [];
 let counter = 1;
 
-/**
+
  GET /users - ดึงข้อมูลผู้ใช้ทั้งหมด
  POST /users - เพิ่มผู้ใช้ใหม่
  GET /users/:id - ดึงข้อมูลผู้ใช้ตาม ID
  PUT /users/:id - แก้ไขข้อมูลผู้ใช้ตาม ID ที่บันทึก
  DELETE /users/:id - ลบผู้ใช้ตาม ID ที่บันทึก
-*/
+
 
 // path: = GET /users
 app.get('/users', (req, res) => {
@@ -93,6 +201,8 @@ app.delete('/users/:id', (req, res) => {
     });
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await initMySQL();
     console.log(`Server is running on http://localhost:${port}`);
 });
+*/
